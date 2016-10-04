9061ea63f9968188be42d4b447448cd6dd61ba41
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-04 09:03:52 +0900

    change the behavior of getoperator

--- operator/GetOperator.ts
+++ operator/GetOperator.ts
@@ -6,6 +6,6 @@ export class GetOperator implements IOperator {
    * call
    */
   public call(engine: Engine, args: Array<any>): Object {
-    return engine.variables[<string>engine.eval(args[0])];
+    return engine.variables[args[0]];
   }
 }
