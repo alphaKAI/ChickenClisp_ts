a717e8e4ae98ec8f2adfb4a812612094fbabdece
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-04 09:25:42 +0900

    update deffun

--- operator/DeffunOperator.ts
+++ operator/DeffunOperator.ts
@@ -11,8 +11,6 @@ export class DeffunOperator implements IOperator {
     var funcArgs: string[] = <string[]>args[1];
     var funcBody: Object   = args[2];
 
-    var x: DynamicOperator = new DynamicOperator(funcArgs, funcBody);
-    engine.variables[funcName] = x;
-    return x;
+    return engine.defineVariable(funcName,  new DynamicOperator(funcArgs, funcBody));
   }
 }
