a3325a9cbd4b50fc4f81eab35312386662be1df9
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-05 18:27:23 +0900

    improve app.ts

--- app.ts
+++ app.ts
@@ -1,5 +1,26 @@
 /// <reference path="./typings/index.d.ts"/>
+import {Engine} from "./Engine";
+import {Transpiler} from "./Transpiler";
 import {Interpreter} from "./Interpreter";
 
-var itpr = new Interpreter();
-itpr.interpreter();
+var flag: boolean = false;
+var fs = require("fs");
+var path = require("path");
+
+if (process.argv.length == 3 && path.basename(process.argv[0]) == "node") {
+  var fpath: string = process.argv[2];
+
+  if (!fs.existsSync(fpath)) {
+    console.log("No such file - " + fpath);
+  } else {
+    var engine: Engine = new Engine();
+    var transpiler: Transpiler = new Transpiler();
+
+    engine.eval(transpiler.transpile(<string>fs.readFileSync(fpath).toString()));
+  }
+} else if (process.argv.length == 1) {
+  var itpr = new Interpreter();
+  itpr.interpreter();
+} else {
+  console.log("error"); // this error comment needs improving
+}
