654fcd7fbdb5fd7738df2f3311e0dbaec87a1ece
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-09 19:42:03 +0900

    fix an indent

--- operator/MapOperator.ts
+++ operator/MapOperator.ts
@@ -9,24 +9,8 @@ export class MapOperator extends Operator implements IOperator {
     var func: IExpression = args[0];
     var eargs1: any       = engine.eval(args[1]);
 
-  if (eargs1 instanceof Array) {
-    var array: Array<any> = eargs1;
-    var ret:   Array<any>  = [];
-    var efunc: Object = engine.eval(func);
-
-    if (efunc instanceof Closure) {
-      array.forEach((elem) => ret.push((<Closure>efunc).eval([elem])));
-    } else {
-      array.forEach((elem) => ret.push((<IOperator>efunc).call(engine, [elem])));
-    }
-
-    return ret;
-  } else {
-      if (!(eargs1 instanceof ImmediateValue) && !(eargs1.value instanceof Array)) {
-        throw new Error("Map requires array and function as a Operator");
-      }
-
-      var array: Array<any> = eargs1.value;
+    if (eargs1 instanceof Array) {
+      var array: Array<any> = eargs1;
       var ret:   Array<any>  = [];
       var efunc: Object = engine.eval(func);
 
@@ -37,6 +21,22 @@ export class MapOperator extends Operator implements IOperator {
       }
 
       return ret;
-    }
+    } else {
+        if (!(eargs1 instanceof ImmediateValue) && !(eargs1.value instanceof Array)) {
+          throw new Error("Map requires array and function as a Operator");
+        }
+
+        var array: Array<any> = eargs1.value;
+        var ret:   Array<any>  = [];
+        var efunc: Object = engine.eval(func);
+
+        if (efunc instanceof Closure) {
+          array.forEach((elem) => ret.push((<Closure>efunc).eval([elem])));
+        } else {
+          array.forEach((elem) => ret.push((<IOperator>efunc).call(engine, [elem])));
+        }
+
+        return ret;
+      }
   }
 }
