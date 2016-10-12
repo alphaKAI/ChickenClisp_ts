77819d00be4ff81be7a12fec3b7dde08244b6a35
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-12 23:47:11 +0900

    fix seq op

--- operator/SeqOperator.ts
+++ operator/SeqOperator.ts
@@ -6,7 +6,7 @@ export class SeqOperator extends Operator implements IOperator {
    * call
    */
   public call(engine: Engine, args: Array<any>): Object {
-    var n: number = <number>args[0];
+    var n: number = <number>engine.eval(args[0]);
     var arr: Array<number> = new Array<number>(n);
 
     for (var i: number = 0; i < n; ++i) {
