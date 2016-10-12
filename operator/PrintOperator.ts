dc6772b6ba6834903808c135cf9aa3c107acb1b2
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-12 23:42:52 +0900

    improve print op

--- operator/PrintOperator.ts
+++ operator/PrintOperator.ts
@@ -10,12 +10,17 @@ export class PrintOperator extends Operator implements IOperator {
   public call(engine: Engine, args: Array<any>): Object {
     args.forEach(arg => {
       var item: Object = engine.eval(arg);
+
+      if (item instanceof Operator) {
+        item = arg;
+      }
+
       if (item instanceof Array) {
         process.stdout.write("(");
         process.stdout.write(item.join(" "))
         process.stdout.write(")");
       } else {
-        process.stdout.write(String(item));
+        process.stdout.write(<string>item);
       }
     });
 
@@ -30,12 +35,17 @@ export class PrintlnOperator extends Operator implements IOperator {
   public call(engine: Engine, args: Array<any>): Object {
     args.forEach(arg => {
       var item: Object = engine.eval(arg);
+
+      if (item instanceof Operator) {
+        item = arg;
+      }
+
       if (item instanceof Array) {
         process.stdout.write("(");
         process.stdout.write(item.join(" "))
         process.stdout.write(")");
       } else {
-        process.stdout.write(String(item));
+        process.stdout.write(<string>item);
       }
     });
 
